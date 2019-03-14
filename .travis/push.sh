b65c97eed4a71b44d00ca20a8d9c4c34acd80683
Andrew Yates <andyyatz@gmail.com>
2019-03-14 18:38:07 +0100

    Support travis-ci based ontology site building

---
+++ .travis/push.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+setup_git() {
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "Travis CI"
+}
+
+build_ontology() {
+  cd src/ontology
+  make
+  make prepare_release
+  cd ../..
+}
+
+commit_files() {
+  git add . ensembl_ontology.o*
+  git commit --message "Travis build: $TRAVIS_BUILD_NUMBER [skip travis]"
+}
+
+upload_files() {
+  git remote add origin-upload https://${GH_TOKEN}@github.com/Ensembl/ensembl-glossary.git > /dev/null 2>&1
+  git push --quiet --set-upstream origin-upload master > /dev/null 2>&1
+}
+
+setup_git
+build_ontology
+commit_files
+upload_files
