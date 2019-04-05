a9f6b00e512b04c10caa49ff03846fd0886041c0
Andrew Yates <andrewyatz@users.noreply.github.com>
2019-04-05 15:59:36 +0100

    Trying to get away from the detached head problem

--- .travis/push.sh
+++ .travis/push.sh
@@ -1,28 +1,62 @@
-#!/bin/sh
+#!/bin/bash
 
-setup_git() {
+function msg() {
+  echo "travis-commit: $*"
+}
+
+function err() {
+  msg "$*" 1>&2
+}
+
+function setup_git() {
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "Travis CI"
 }
 
-build_ontology() {
+function build_ontology() {
   cd src/ontology
   make
   make prepare_release
   cd ../..
 }
 
-commit_files() {
+function commit_files() {
+  # Using https://gist.github.com/ddgenome/f3a60fe4c2af0cbe758556d982fbeea9 to get onto a real branch 
+  # and not detached HEAD
+  if ! git checkout "$TRAVIS_BRANCH"; then
+    err "failed to checkout $TRAVIS_BRANCH"
+    return 1
+  fi
+  local git_tag=SOME_TAG_TRAVIS_WILL_NOT_BUILD+travis$TRAVIS_BUILD_NUMBER
+  if ! git tag "$git_tag" -m "Generated tag from Travis CI build $TRAVIS_BUILD_NUMBER"; then
+    err "failed to create git tag: $git_tag"
+    return 1
+  fi
+  
   git add ensembl-glossary.obo ensembl-glossary.owl
-  git commit --message "Travis build: $TRAVIS_BUILD_NUMBER [skip travis]"
+  git commit --message "Travis build: $TRAVIS_BUILD_NUMBER [ci skip]"
 }
 
-upload_files() {
+function upload_files() {
+  if [[ $TRAVIS_BRANCH != master ]]; then
+    msg "not pushing updates to branch $TRAVIS_BRANCH"
+    return 0
+  fi
   git remote add origin-upload https://${GH_TOKEN}@github.com/Ensembl/ensembl-glossary.git > /dev/null 2>&1
-  git push --quiet --set-upstream origin-upload master > /dev/null 2>&1
+  if ! git push --quiet --follow-tags --set-upstream origin-upload "$TRAVIS_BRANCH" > /dev/null 2>&1; then
+    err "failed to push git changes"
+    return 1
+  fi
 }
 
 setup_git
+
 build_ontology
-commit_files
-upload_files
+
+if ! commit_files; then
+  exit 1
+fi
+
+if ! upload_files; then
+  exit 1
+fi
